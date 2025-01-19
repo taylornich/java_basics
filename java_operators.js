b189165f5b04b73017083209f77d329bd2e6ef6d
taylornich <166781921+taylornich@users.noreply.github.com>
2025-01-18 18:53:29 -0700

    Question 2

---
+++ java_operators.js
@@ -0,0 +1,27 @@
+//Question 2 task 1 and 2
+var x, y;
+x = 28;
+y = 69;
+
+//Question 2 Task 3
+console.log("Sum:", x + y);
+console.log("Difference:", x - y);
+console.log("Product:", x * y);
+console.log("Quotient:", x / y);
+
+//Question 2 Task 4
+x += 12;
+y -= 23;
+console.log("Updated x value:", x)
+console.log("Updated y value:", y)
+
+//Question 2 Task 5
+console.log("Is x equal to y?", x == y);
+console.log("Is x greater then y?", x > y);
+console.log("Is x not equal to y?", x != y);
+
+//Question 2 Task 6
+let isPositive = (x > 0 && y > 0);
+let isEven = (x % 2 === 0 && y % 2 === 0);
+console.log("Are both numbers positive?", isPositive);
+console.log("Are both numbers even?", isEven);

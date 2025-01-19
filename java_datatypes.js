fd6cd1690201d458bdd33fc66717cad50e7f7773
taylornich <166781921+taylornich@users.noreply.github.com>
2025-01-18 18:45:57 -0700

    Questions 1 and 2

---
+++ java_datatypes.js
@@ -0,0 +1,11 @@
+//Question 1 Task 1 and 2
+
+var studentName, studentAge, gradeAverage, attendedSchool;
+
+studentName = "Brianna";
+studentAge = 10;
+gradeAverage = "A";
+attendedSchool = true;
+
+//Question 1 Task 3
+console.log(studentName, studentAge, gradeAverage, attendedSchool)
